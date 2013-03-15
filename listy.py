a385bb4c4ffcd2252c847c2e7e44f9543cab261d
unknown <solomon.fc@gmail.com>
2013-03-15 17:12:32 +0800

    update tasklist

--- listy.py
+++ listy.py
@@ -1,13 +1,11 @@
 import webapp2
 import os
+import settings
 
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
-
 from apiclient.discovery import build
 from oauth2client.appengine import OAuth2Decorator
-
-import settings
 
 decorator = OAuth2Decorator(
 	client_id=settings.CLIENT_ID,
@@ -16,39 +14,34 @@ decorator = OAuth2Decorator(
 
 service = build('tasks', 'v1')
 
-class MainHandler(webapp2.RequestHandler):
+class TaskLists(webapp2.RequestHandler):
 
 	@decorator.oauth_required
 	def get(self):
-		tasks = service.tasks().list(tasklist='@default').execute(http=decorator.http())
+		tasklists = service.tasklists().list().execute(http=decorator.http())
+		path = os.path.join(os.path.dirname(__file__), 'taskList.html')
+		self.response.out.write(template.render(path, tasklists))
 
-		template_values = {
-			'tasks': tasks['items'],
-		}
+class Tasks(webapp2.RequestHandler):
+	
+	@decorator.oauth_required
+	def get(self):
+		tasklistID = self.request.GET['id']
+		
+		tasklist = service.tasklists().get(tasklist=tasklistID).execute(http=decorator.http())
+		tasks = service.tasks().list(tasklist=tasklistID).execute(http=decorator.http())
+		
+		values = { 
+						'tasklist': tasklist, 
+						'tasks': tasks['items']
+					}
 
 		path = os.path.join(os.path.dirname(__file__), 'tasks.html')
-		self.response.out.write(template.render(path, template_values))
-
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-
-		template_values = {
-				'url': "http://www.google.com.hk",
-				'url_linktext': "Google HK",
-		}
-
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, template_values))
-
-class Listy(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('!!!Listy!!!')
+		self.response.out.write(template.render(path, values))
 
 app = webapp2.WSGIApplication([
-			('/', MainHandler),
-			('/page', MainPage), 
-			("/listy", Listy),
+			('/', TaskLists),
+			('/tasks', Tasks),
 			(decorator.callback_path, decorator.callback_handler())
 	],debug=True)
 
