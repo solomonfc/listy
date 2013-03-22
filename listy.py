21f582727610ce6da3c24fc7738c28d39fb6d740
solomon_fc <solomon.fc@gmail.com>
2013-03-22 08:51:34 +0800

    unknown

--- listy.py
+++ listy.py
@@ -4,6 +4,29 @@ import os
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
+from apiclient.discovery import build
+from oauth2client.appengine import OAuth2Decorator
+
+import settings
+
+decorator = OAuth2Decorator(client_id=settings.CLIENT_ID,
+														client_secret=settings.CLIENT_SECRET,
+														scope=settings.SCOPE)
+
+service = build('tasks', 'v1')
+
+
+class MainHandler(webapp2.RequestHandler):
+
+	@decorator.oauth_required
+	def get(self):
+		tasks = service.tasks().list(tasklist='@default').execute(
+			http=decorator.http())
+		self.response.write('<html><body><ul>')
+		for task in tasks['items']:
+			self.response.write('<li>%s</li>' % task['title'])
+		self.response.write('</ul></body><html>')
+
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 
@@ -21,8 +44,10 @@ class Listy(webapp2.RequestHandler):
 		self.response.write('!!!Listy!!!')
 
 app = webapp2.WSGIApplication([
-		('/', MainPage), 
-		("/listy", Listy)
+			('/', MainHandler),
+			(decorator.callback_path, decorator.callback_handler()),
+			('/Page', MainPage), 
+			("/listy", Listy)
 	],debug=True)
 
 def main():
